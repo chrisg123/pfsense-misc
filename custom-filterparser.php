b7186a657a409a07d789da040e3218bdd23dd97a
Charlie Root <root@heimdal.localdomain>
2019-02-02 00:42:30 -0500

    More formatting.

--- custom-filterparser.php
+++ custom-filterparser.php
@@ -34,17 +34,33 @@ include_once("filter_log.inc");
 
 $log = fopen("php://stdin", "r");
 $lastline = "";
+
 while (!feof($log)) {
-	$line = fgets($log);
-	$line = rtrim($line);
-	$flent = parse_firewall_log_line(trim($line));
-    
-	if ($flent != "") {
-		$flags = (($flent['proto'] == "TCP") && !empty($flent['tcpflags'])) ? ":" . $flent['tcpflags'] : "";
-        $color = ($flent['act'] == "block") ? ":" . "31m" : "32m";
-		//echo "{$flent['time']} {$flent['act']} {$flent['realint']} {$flent['proto']}{$flags} {$flent['src']} {$flent['dst']}\n";
-        echo "{$flent['time']} \033[1;{$color}{$flent['act']}\033[m  {$flent['direction']} {$flent['interface']} {$flent['src']} {$flent['proto']}{$flags} {$flent['dst']}\n";
-		$flent = "";
-	}
+    $line = fgets($log);
+    $line = rtrim($line);
+    $flent = parse_firewall_log_line(trim($line));
+    if ($flent != "") {
+        $flags = (($flent['proto'] == "TCP") && !empty($flent['tcpflags'])) ? ":" . $flent['tcpflags'] : "";
+        //echo "{$flent['time']} {$flent['act']} {$flent['realint']} {$flent['proto']}{$flags} {$flent['src']} {$flent['dst']}\n";
+
+        // Start: Customized filter parsing
+        $actColor = ($flent['act'] == "block") ? ":" . "31m" : "32m";
+        $directionFmt = ($flent['direction'] == "in") ? "1;" : "";
+
+        $src = gethostbyaddr($flent['srcip']);
+        $dst = gethostbyaddr($flent['dstip']);
+        $direction = ($flent['direction'] == "out") ? "--> " : "<-- ";
+        usleep(750000); // slow it down for human readability
+        echo "\033[1;30m{$flent['time']}\033[m" .
+             "\033[1;{$actColor} " . str_pad($flent['act'], 6) . "\033[m" .
+             "\033[{$directionFmt}35m{$direction}\033[m" .
+             " " . str_pad($flent['interface'], 10) .
+             " \033[1;30m{$src}:{$flent['srcport']}\033[m" .
+             " \033[1;34m{$flent['proto']}\033[m{$flags} \033[1;30m{$dst}:{$flent['dstport']}\033[m\n";
+
+        // END: Customized filter parsing
+
+        $flent = "";
+    }
 }
 fclose($log); ?>
